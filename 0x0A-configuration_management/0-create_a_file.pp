95b6764a07ed6cf8bc6328a3625e09dd5768cea5
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-08-19 20:50:37 -0500

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,2 +1,9 @@
 # A file is created in the path of 'file'
 
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
