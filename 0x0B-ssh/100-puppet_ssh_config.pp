91ce2410327b63b74d75d04bc7911fe8fe254957
Stewardvr93 <svaldezr93@gmail.com>
2022-08-24 20:10:53 -0500

    understanding little by little

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Let’s practice using Puppet to make changes to our configuration file.
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+  }
+  -> exec { 'Append IdentityFile':
+    command => '/usr/bin/echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+    }
+    -> exec { 'Append PasswordAuthentication':
+      command => '/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config'
+      }
+      
