2ad42dfd3576f29fd08bb862e8b07ef0b7178609
Stewardvr93 <svaldezr93@gmail.com>
2022-09-09 11:37:29 -0500

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API,
+and return information about the progress of your TODO list.."""
+if __name__ == "__main__":
+        
+        import requests
+        import sys
+        did = sys.argv[1]
+        url = request.get("https://jsonplaceholder.typicode.com/users/{}"
+                          .format(did))
+        nombre
